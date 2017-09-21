7fb9ff8cc5dbd12df69f36db348b6c94c6591ad1
spookykidmm <spookykidmm@gmail.com>
2017-09-20 20:42:27 -0400

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 pwd=$(zenity --entry --text="Please enter your password" --hide-text)
 echo $pwd | sudo -S pacman -S --noconfirm git pacaur
-pacaur -S rofi clipmenu i3-gaps network-manager-applet nm-connection-editor conky conky-manager light 
-playerctl python-pytz python-tzlocal pamixer acpi lxsession-gtk3 scrot dunst i3status py3status feh compton-garnetius-git cower paranoid python-pydbus oblogout i3lock bumblebee-status-git i3status-rust-git ttf-font-awesome nerd-fonts-complete
+pacaur -S rofi clipmenu i3-gaps network-manager-applet nm-connection-editor conky conky-manager light playerctl python-pytz python-tzlocal pamixer acpi lxsession-gtk3 scrot dunst mate-notification-daemon kdeconnect indicator-kdeconnect i3status py3status feh compton-garnetius-git cower paranoid python-pydbus oblogout i3lock bumblebee-status-git i3status-rust-git ttf-font-awesome nerd-fonts-complete
 # git clone https://github.com/spookykidmm/spooky-i3
 cd spooky-i3
 mkdir -p ~/.config/i3/config
