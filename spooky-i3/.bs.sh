82d7e621a3f083c8fe7e568d26170db55cf5feda
spookykidmm <spookykidmm@gmail.com>
2017-11-01 19:37:56 -0400

    Cleaned up config

--- spooky-i3/.bs.sh
+++
@@ -1,1 +0,0 @@
-bumblebee-status -m datetime battery brightness weather -p interval="5" weather.apikey="4361e8a5c278a83c24394e6f8c47c3e3" weather.unit="imperial" -t solarized-dark-awesome
--- spooky-i3/bl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-heck=$(zenity --scale --text="What would you like to set your backlight brightness to?")
-light -S $heck
